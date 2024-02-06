b45219e091d5933b3d807c6aa7da6a701f1d9b67
Eduardo Bombonatto <eduardoblorenzetti517@gmail.com>
2024-02-06 16:28:42 -0300

    pagina principal

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    print("Bem vindo ao sistema da loja Yorick Coveiro fudido!")
+    print("Escolha uma opção")
+    print("Numero | Ação")
+    print("------------------------")
+    print("1      | Produtos")
+    print("2      | Clientes")
+    print("3      | Vendas")
+    print("4      | Usuários")
+    print('\n')
+    print("0      | Sair do sistema")
+
+if __name__ == '__main__':
+    main()
